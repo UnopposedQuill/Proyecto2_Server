6348d8a2e35a16f4f3adfd5d9ff07f80e2f3926b
UnopposedQuill <estgusa23@gmail.com>
2025-01-31 17:43:19 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Launch
+
+`nodemon` does not work on this project.
+Instead relying on
+
+`npx ts-node .\src\server.ts`
+
+For unknown typescript reasons.
---
+++ src/server.ts
@@ -0,0 +1,49 @@
+import mongoose, { mongo, Mongoose } from "mongoose";
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (request: Request, response: Response, next: () => any) => {
+  response.status(200).json({ message: "Server ping response" });
+});
+
+/**
+ * Responses for Movies
+ */
+app.get("/movies", (request: Request, response: Response, next: () => any) => {
+  response.status(200).json({ message: "Movies response" });
+});
+
+app.get(
+  "/movies/:id",
+  (request: Request, response: Response, next: () => any) => {
+    response
+      .status(200)
+      .json({ message: "Specific movie response", id: request.params.id });
+  }
+);
+
+/**
+ * Responses for Actors
+ */
+app.get("/actors", (request: Request, response: Response, next: () => any) => {
+  response.status(200).json({ message: "Actors response" });
+});
+
+/**
+ * Responses for Users
+ * TODO
+ */
+
+// const model = mongoose.model();
+
+const main = async () => {
+  app.listen(port, () => {
+    console.log("Received request: Request");
+  });
+};
+
+main();
